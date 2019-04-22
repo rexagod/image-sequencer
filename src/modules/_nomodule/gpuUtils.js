e913640eb60eea2476b8c4536470f7e8e833f70f
HarshKhandeparkar <mjkgoa@gmail.com>
2019-04-22 20:50:50 +0530

    add gpuUtil convolve

--- src/modules/_nomodule/gpuUtils.js
+++ src/modules/_nomodule/gpuUtils.js
@@ -9,21 +9,47 @@ const gpu = new GPU({mode: 'gpu'})
  * @param {Boolean} texMode Whether to save the output to a texture.
  * @returns {Float32Array} 
  */
-const convolve = (array, kernel, texMode = false, normailize = false) => {
+const convolve = (array, kernel, texMode = false, normalize = false) => {
+  const arrayX = array[0].length,
+    arrayY = array.length,
+    kernelX = kernel[0].length,
+    kernelY = kernel.length,
+    paddingX = kernelX - 2,
+    paddingY = kernelY - 2;
+
   const matConvFunc = `function (array, kernel) {
     var sum = 0;
-    for (var i = 0; i < 3; i++){
-      for (var j = 0; j < 3; j++){
+    for (var i = 0; i < ${kernelX}; i++){
+      for (var j = 0; j < ${kernelY}; j++){
         sum += kernel[j][i] * array[this.thread.y + j][this.thread.x + i]
       }
     }
     return sum;
   }`;
   
-    const paddingFunc = `function(array, paddingSize) {
-    const positionX = Math.min(Math.max(this.thread.x - paddingSize, 0), ${sizeX} - 1)
-    const positionY = Math.min(Math.max(this.thread.y - paddingSize, 0), ${sizeY} - 1)
+    const paddingFunc = `function(array) {
+    const positionX = Math.min(Math.max(this.thread.x - ${paddingX}, 0), ${arrayX} - 1);
+    const positionY = Math.min(Math.max(this.thread.y - ${paddingY}, 0), ${arrayY} - 1);
   
-    return array[positionY][positionX]
+    return array[positionY][positionX];
   }`;
+
+  const padIt = gpu.createKernel(paddingFunc, {
+    output: [arrayX + paddingX, arrayY + paddingY],
+    outputToTexture: true
+  })
+
+  const convolveKernel = gpu.createKernel(matConvFunc, {
+    output: [arrayX, arrayY],
+    outputToTexture: texMode
+  })
+
+  padIt.build(array);
+
+  const paddedArray = padIt(array)
+
+  convolveKernel.build(paddedArray, kernel)
+
+  if (texMode) return convolveKernel(paddedArray, kernel).toArray()
+  else return convolveKernel(paddedArray, kernel)
 }
