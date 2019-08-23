3eddd2a0514b7e5228ffde9f8bcaa9a9c3409ffd
rexagod <rexagod@gmail.com>
2019-08-23 23:23:52 +0530

    syncing in order to migrate to dif sys

--- src/modules/Matcher/Module.js
+++ src/modules/Matcher/Module.js
@@ -2,42 +2,51 @@
  * Match the images
  */
 function Match(options, UI) {
-
-  var output, points;
-
-  var defaults = require('./../../util/getDefaults.js')(require('./info.json'));
-
-  global.XY = [
-    options.imageX || defaults.imageX,
-    options.imageY || defaults.imageY
-  ];
-
-  const core = require('matcher-core');
-
-  core(function(err, out, code){
-    points = JSON.parse(out.trim());
-  });
+  var output;
 
   function draw(input, callback, progressObj) {
-
     progressObj.stop(true);
     progressObj.overrideFlag = true;
 
     var step = this;
-
-    function changePixel(r, g, b, a, x, y) {
-      for(var i = 0; i < 500; i++){
-        if(Math.abs(points[i].x - x) <= 10 && Math.abs(points[i].y - y) <= 10){
-          return [0, 255, 0];
-        }
+    async function asf() {
+      if(this.results){
+        return this.results;
       }
-      return [r, g, b, a];
+      new Matcher(
+        "examples/images/small.jpg",
+        "examples/images/big.jpg",
+        async function(q) {
+          var res = await q;
+          this.results = res.points;
+        }
+      );
+      return await this.results;
+    }
+
+    async function changePixel(r, g, b, a, x, y) {
+      debugger
+      asf().then(function(points){
+
+        for (var i = 0; i < 500; i++) {
+          if (
+            Math.abs(points[i].x - x) <= 10 &&
+            Math.abs(points[i].y - y) <= 10
+          ) {
+            return [0, 255, 0];
+          }
+        }
+
+        var rgba = [r, g, b, a];
+        this.rgba = rgba;
+      });
+
+      return await this.rgba;
+
     }
 
     function output(image, datauri, mimetype) {
-
       step.output = { src: datauri, format: mimetype };
-
     }
 
     return input.pixelManipulation({
@@ -47,9 +56,8 @@ function Match(options, UI) {
       image: options.image,
       inBrowser: options.inBrowser,
       callback: callback,
-      useWasm:options.useWasm
+      useWasm: options.useWasm
     });
-
   }
 
   return {
